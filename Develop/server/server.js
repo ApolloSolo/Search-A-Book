f856c5e14799e3ff587212547a5c3a773a927310
ApolloSolo <matt1teixeira@gmail.com>
2022-06-08 17:13:53 -0400

    Installed Apollo and GraphQL

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -19,3 +19,15 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+
+// To fulfill the Challenge, you’ll need to do the following:
+
+// Set up an Apollo Server to use GraphQL queries and mutations to fetch and modify data, replacing the existing RESTful API.
+
+// Modify the existing authentication middleware so that it works in the context of a GraphQL API.
+
+// Create an Apollo Provider so that requests can communicate with an Apollo Server.
+
+// Deploy the application to Heroku.
